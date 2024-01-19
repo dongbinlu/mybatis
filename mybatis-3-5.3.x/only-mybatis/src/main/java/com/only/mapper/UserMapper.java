2952e32426eba5919fe1c199e30aba0970296dcd
ludongbin <v_boys@163.com>
2024-01-19 17:05:26 +0800

    mybatis初始化

---
+++ mybatis-3-5.3.x/only-mybatis/src/main/java/com/only/mapper/UserMapper.java
@@ -0,0 +1,14 @@
+package com.only.mapper;
+
+import com.only.entity.User;
+
+/***
+ * @Author 徐庶   QQ:1092002729
+ * @Slogan 致敬大师，致敬未来的你
+ */
+public interface UserMapper {
+
+    User selectById(Integer id);
+
+    void updateForName(String id,String username);
+}
